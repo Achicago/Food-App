600a7d785fa482b8b6d31e9bed6a35c8891ff3be
achicago <achigonye@gmail.com>
2023-03-10 17:15:50 +0100

    navigation setup

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import CategoriesScreen from './screens/CategoriesScreen';
+import MealsOverviewScreen from './screens/MealsOverviewScreen';
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator();
+
   return (
     <>
       <StatusBar style='light' />
       <NavigationContainer>
-        <CategoriesScreen />
+        <Stack.Navigator>
 
+          <Stack.Screen name='MealsCategories' component={CategoriesScreen} />
+          <Stack.Screen name='MealOverview' component={MealsOverviewScreen} />
+        </Stack.Navigator>
+        {/* <CategoriesScreen /> */}
       </NavigationContainer>
     </>
   );
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -2,11 +2,26 @@ import { FlatList } from "react-native";
 import CategoryGridTile from "../components/CategoryGridTile";
 import { CATEGORIES } from "../data/dummy-data";
 
-function renderCategoryItem(itemData) {
-    return <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />;
-}
 
-function CategoriesScreen() {
+
+function CategoriesScreen({ navigation }) {
+
+    function renderCategoryItem(itemData) {
+
+        function pressHandler() {
+            navigation.navigate('MealOverview', {
+                categoryId: itemData.item.id,
+            });
+        }
+
+        return (
+            <CategoryGridTile
+                title={itemData.item.title}
+                color={itemData.item.color}
+                onPress={pressHandler}
+            />
+        );
+    }
 
     return <FlatList
         data={CATEGORIES}
