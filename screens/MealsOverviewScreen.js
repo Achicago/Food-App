4d688138d38b6e74068bba1065c535f800ceb307
achicago <achigonye@gmail.com>
2023-03-13 07:57:40 +0100

    Managing Favorite Meals with contex Api

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,7 +1,8 @@
 
 import { useLayoutEffect } from "react";
-import { Text, View, StyleSheet, FlatList } from "react-native";
-import MealItem from "../components/MealItem";
+// import { Text, View, StyleSheet, FlatList } from "react-native";
+// import MealItem from "../components/MealsList/MealItem";
+import MealsList from "../components/MealsList/MealsList";
 import { MEALS, CATEGORIES } from "../data/dummy-data";
 
 function MealsOverviewScreen({ route, navigation }) {
@@ -21,41 +22,9 @@ function MealsOverviewScreen({ route, navigation }) {
 
     }, [catId, navigation])
 
+    return <MealsList items={displayedMeals} />
 
-    function renderMealItem(itemData) {
-        const item = itemData.item;
-
-        const mealItemProps = {
-            id: item.id,
-            imageUrl: item.imageUrl,
-            title: item.title,
-            duration: item.duration,
-            complexity: item.complexity,
-            affordability: item.affordability,
-        }
-
-
-        return <MealItem
-            {...mealItemProps}
-        />;
-    }
-
-    return (
-        <View style={styles.container} >
-            <FlatList
-                data={displayedMeals}
-                renderItem={renderMealItem}
-                keyExtractor={(item) => item.id}
-            />
-        </View>
-    )
 };
 
 export default MealsOverviewScreen;
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 16,
-    }
-});
